b88c2cfe216ef13a60d717b1d1bb143aaa3ec217
MatteoRichardGaudino <matt.gaudino@studenti,unina.it>
2021-01-05 11:22:17 +0100

    Aggiunte alcune finestre e schermate principali

---
+++ src/main/java/ControllerTabellone.java
@@ -0,0 +1,56 @@
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerTabellone implements Initializable {
+
+    @FXML
+    private VBox voloBox;
+    @FXML
+    private VBox destinazioneBox;
+    @FXML
+    private VBox partenzaBox;
+    @FXML
+    private VBox statoBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("ok");
+        for (int i = 0; i < 5; i++) {
+            Label l = new Label("ciao");
+            l.setPrefSize(217, 30);
+            l.setStyle("-fx-background-color: white; -fx-spacing: 2");
+
+            voloBox.getChildren().add(l);
+        }
+        for (int i = 0; i < 5; i++) {
+            Label l = new Label("ciao");
+            l.setPrefSize(217, 30);
+            l.setStyle("-fx-background-color: white; -fx-spacing: 2");
+
+            destinazioneBox.getChildren().add(l);
+        }for (int i = 0; i < 5; i++) {
+            Label l = new Label("ciao");
+            l.setPrefSize(217, 30);
+            l.setStyle("-fx-background-color: white; -fx-spacing: 2");
+
+            partenzaBox.getChildren().add(l);
+        }for (int i = 0; i < 5; i++) {
+            Label l = new Label("ciao");
+            l.setPrefSize(217, 30);
+            l.setStyle("-fx-background-color: white; -fx-spacing: 2");
+
+            statoBox.getChildren().add(l);
+        }
+
+
+    }
+}
---
+++ src/main/java/ControllerTratte.java
@@ -0,0 +1,46 @@
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXRippler;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.FlowPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerTratte implements Initializable {
+
+    @FXML
+    private JFXComboBox<String> searchMode;
+    private FlowPane scrollPan;
+    @FXML
+    private ScrollPane scroll;
+
+    public void close(MouseEvent e){
+        Platform.exit();
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        searchMode.getItems().addAll( "Citta'", "Compagnia");
+        scrollPan = new FlowPane();
+        scrollPan.setHgap(20);
+        scrollPan.setVgap(15);
+        scrollPan.setStyle("-fx-background-color: transparent");
+        for (int i = 0; i < 20; i++) {
+            JFXButton l =  new JFXButton("ciao");
+            l.setPrefSize(260, 200);
+
+            if (i%2 == 0)
+                l.setStyle("-fx-background-color: red; -fx-background-radius: 25;");
+            else
+                l.setStyle("-fx-background-color: blue; -fx-background-radius: 25;");
+            scrollPan.getChildren().add(l);
+        }
+        scroll.setContent(scrollPan);
+    }
+}
